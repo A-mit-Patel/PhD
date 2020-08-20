20a7954af8e807125d793a0777ca9114eb1d8357
Amit Patel <55692909+A-mit-Patel@users.noreply.github.com>
2020-08-20 08:20:17 +0100

    Adept Chapter Updated Simulations
    Ran the modified simulations specifically for the design which had no follow up and a cohort size of 1.
    
    !! I should probably run this again so each patient has completed treatment before another is recruited !!
    
    Created code to generate a table and included it in the main text.

--- Thesis/Chapters/Chapter2/ADePT/Modified Simulations.R
+++ Thesis/Chapters/Chapter2/ADePT/Modified Simulations.R
@@ -35,7 +35,9 @@ tox.lim <- 0.35
 # The probability value to be used when assessing the certainty required that toxicty at the specificed dose exceeds tox_lim
 tox.cert <- 0.8
 
-
+################################################################################
+################################################################################
+################################################################################
 # Modification 1 Sample size of 30 patients - No stop for consensus
 
 # Number of patients used to define stopping rule
@@ -111,6 +113,9 @@ mod30_s8 <- titepocrm_sim(r, alpha, prior.o, x0, stop, n, theta, nsim,
                         mtd.lim, tox.lim, tox.cert)
 mod30_s8
 
+################################################################################
+################################################################################
+################################################################################
 
 # Modification 2 Sample size of 60 patients - No stop for consensus
 
@@ -187,8 +192,11 @@ mod60_s8 <- titepocrm_sim(r, alpha, prior.o, x0, stop, n, theta, nsim,
                           mtd.lim, tox.lim, tox.cert)
 mod60_s8
 
+################################################################################
+################################################################################
+################################################################################
 
-# Modification 3 Removal of cohorts 
+# Modification 3 Removal of cohorts and minimum follow up  
 # Number of patients used to define stopping rule
 stop <- 61
 # Maximum sample size.
@@ -200,7 +208,7 @@ mtd.lim <- 15
 
 r <- c(0.25, 0.40, 0.45, 0.50, 0.55, 0.60)
 set.seed(101)
-modco_s1 <- titepocrm_sim(r, alpha, prior.o, x0, stop, n, theta, nsim, 
+modco_s1 <- titepocrm_sim_nomin(r, alpha, prior.o, x0, stop, n, theta, nsim, 
                           tox.range, cohort, obswin, minfu, win2, recrate,
                           mtd.lim, tox.lim, tox.cert)
 modco_s1
@@ -208,7 +216,7 @@ modco_s1
 # True toxicity rates of scenario 2
 r <- c(0.12 ,0.25, 0.40, 0.45, 0.50, 0.55)
 set.seed(102)
-modco_s2 <- titepocrm_sim(r, alpha, prior.o, x0, stop, n, theta, nsim, 
+modco_s2 <- titepocrm_sim_nomin(r, alpha, prior.o, x0, stop, n, theta, nsim, 
                           tox.range, cohort, obswin, minfu, win2, recrate,
                           mtd.lim, tox.lim, tox.cert)
 modco_s2
@@ -216,7 +224,7 @@ modco_s2
 # True toxicity rates of scenario 3
 r <- c(0.09 ,0.12 ,0.25, 0.40, 0.45, 0.50)
 set.seed(103)
-modco_s3 <- titepocrm_sim(r, alpha, prior.o, x0, stop, n, theta, nsim, 
+modco_s3 <- titepocrm_sim_nomin(r, alpha, prior.o, x0, stop, n, theta, nsim, 
                           tox.range, cohort, obswin, minfu, win2, recrate,
                           mtd.lim, tox.lim, tox.cert)
 modco_s3
@@ -224,7 +232,7 @@ modco_s3
 # True toxicity rates of Scenario 4
 r <- c(0.06, 0.09, 0.12, 0.25, 0.40, 0.45)
 set.seed(104)
-modco_s4 <-titepocrm_sim(r, alpha, prior.o, x0, stop, n, theta, nsim, 
+modco_s4 <-titepocrm_sim_nomin(r, alpha, prior.o, x0, stop, n, theta, nsim, 
                          tox.range, cohort, obswin, minfu, win2, recrate,                                
                          mtd.lim, tox.lim, tox.cert)
 modco_s4
@@ -232,7 +240,7 @@ modco_s4
 # True toxicity rates of scenario 5
 r <- c(0.03, 0.06, 0.09, 0.12 ,0.25, 0.40)
 set.seed(105)
-modco_s5 <- titepocrm_sim(r, alpha, prior.o, x0, stop, n, theta, nsim, 
+modco_s5 <- titepocrm_sim_nomin(r, alpha, prior.o, x0, stop, n, theta, nsim, 
                           tox.range, cohort, obswin, minfu, win2, recrate,
                           mtd.lim, tox.lim, tox.cert)
 modco_s5
@@ -240,7 +248,7 @@ modco_s5
 # True toxicity rates of scenario 6
 r <- c(0.01, 0.03, 0.06, 0.09, 0.12 ,0.25)
 set.seed(106)
-modco_s6 <- titepocrm_sim(r, alpha, prior.o, x0, stop, n, theta, nsim, 
+modco_s6 <- titepocrm_sim_nomin(r, alpha, prior.o, x0, stop, n, theta, nsim, 
                           tox.range, cohort, obswin, minfu, win2, recrate,
                           mtd.lim, tox.lim, tox.cert)
 modco_s6
@@ -248,7 +256,7 @@ modco_s6
 # True toxicity rates of scenario 7
 r <- c(0.05, 0.10, 0.15, 0.20 ,0.25 ,0.30)
 set.seed(107)
-modco_s7 <- titepocrm_sim(r, alpha, prior.o, x0, stop, n, theta, nsim, 
+modco_s7 <- titepocrm_sim_nomin(r, alpha, prior.o, x0, stop, n, theta, nsim, 
                           tox.range, cohort, obswin, minfu, win2, recrate,
                           mtd.lim, tox.lim, tox.cert)
 modco_s7
@@ -256,7 +264,7 @@ modco_s7
 # True toxicity rates when all too toxic
 r <- c(0.50, 0.60, 0.65, 0.70 ,0.75 ,0.8)
 set.seed(108)
-modco_s8 <- titepocrm_sim(r, alpha, prior.o, x0, stop, n, theta, nsim, 
+modco_s8 <- titepocrm_sim_nomin(r, alpha, prior.o, x0, stop, n, theta, nsim, 
                           tox.range, cohort, obswin, minfu, win2, recrate, 
                           mtd.lim, tox.lim, tox.cert)
 modco_s8
--- Thesis/Chapters/Chapter2/ADePT/TABLES.R
+++ Thesis/Chapters/Chapter2/ADePT/TABLES.R
@@ -2,7 +2,7 @@ load('ADePT-DDR_OC.RData')
 library(kableExtra)
 
 ################################################################################
-# Need to change columnt that says MTD to TD25 if i ever run again. 
+# ADePT-DDR Full simulations 
 ################################################################################
 
 temp <- kable(OC_tab, "latex", booktabs = T, linesep = "", align = "c", 
@@ -26,3 +26,104 @@ Probability of selecting a dose as the TD25.") %>%
   collapse_rows(columns = 1:2, latex_hline = "major",  valign = "middle") 
 
 cat(temp2)
+
+
+################################################################################
+# Comparison Tables
+################################################################################
+# Load all data
+load('ADePT-DDR_OC.RData')
+load('TITE_sims.RData')
+load('PO-CRM_sims.RData')
+load('Modified_sims.RData')
+
+# Table with selection probabilities
+SelectProb <- round(rbind(round(c(skeleton, 0, 0),2), c(fit_s1$true.prob, 0, 0),
+      c(fit_s1$MTD.selection, fit_s1$stop, fit_s1$months), 
+      c(s1$summary$mtd, as.numeric(s1$summary$prob_stop[2]), s1$summary$months),
+      c(po_s1$MTD.selection, po_s1$stop, po_s1$months), 
+      c(mod30_s1$MTD.selection, mod30_s1$stop, mod30_s1$months),
+      c(mod60_s1$MTD.selection, mod60_s1$stop, mod60_s1$months),
+      c(modco_s1$MTD.selection, modco_s1$stop, modco_s1$months), 
+      c(fit_s2$true.prob, 0, 0),
+      c(fit_s2$MTD.selection, fit_s2$stop, fit_s2$months), 
+      c(s2$summary$mtd, as.numeric(s2$summary$prob_stop[2]), s2$summary$months),
+      c(po_s2$MTD.selection, po_s2$stop, po_s2$months), 
+      c(mod30_s2$MTD.selection, mod30_s2$stop, mod30_s2$months),
+      c(mod60_s2$MTD.selection, mod60_s2$stop, mod60_s2$months),
+      c(modco_s2$MTD.selection, modco_s2$stop, modco_s2$months),
+      c(fit_s3$true.prob, 0, 0),
+      c(fit_s3$MTD.selection, fit_s3$stop, fit_s3$months), 
+      c(s3$summary$mtd, as.numeric(s3$summary$prob_stop[2]), s3$summary$months),
+      c(po_s3$MTD.selection, po_s3$stop, po_s3$months), 
+      c(mod30_s3$MTD.selection, mod30_s3$stop, mod30_s3$months),
+      c(mod60_s3$MTD.selection, mod60_s3$stop, mod60_s3$months),
+      c(modco_s3$MTD.selection, modco_s3$stop, modco_s3$months),
+      c(fit_s4$true.prob, 0, 0),
+      c(fit_s4$MTD.selection, fit_s4$stop, fit_s4$months), 
+      c(s4$summary$mtd, as.numeric(s4$summary$prob_stop[2]), s4$summary$months),
+      c(po_s4$MTD.selection, po_s4$stop, po_s4$months), 
+      c(mod30_s4$MTD.selection, mod30_s4$stop, mod30_s4$months),
+      c(mod60_s4$MTD.selection, mod60_s4$stop, mod60_s4$months),
+      c(modco_s4$MTD.selection, modco_s4$stop, modco_s4$months),
+      c(fit_s5$true.prob, 0, 0),
+      c(fit_s5$MTD.selection, fit_s5$stop, fit_s5$months), 
+      c(s5$summary$mtd, as.numeric(s5$summary$prob_stop[2]), s5$summary$months),
+      c(po_s5$MTD.selection, po_s5$stop, po_s5$months), 
+      c(mod30_s5$MTD.selection, mod30_s5$stop, mod30_s5$months),
+      c(mod60_s5$MTD.selection, mod60_s5$stop, mod60_s5$months),
+      c(modco_s5$MTD.selection, modco_s5$stop, modco_s5$months),
+      c(fit_s6$true.prob, 0, 0),
+      c(fit_s6$MTD.selection, fit_s6$stop, fit_s6$months), 
+      c(s6$summary$mtd, as.numeric(s6$summary$prob_stop[2]), s6$summary$months),
+      c(po_s6$MTD.selection, po_s6$stop, po_s6$months), 
+      c(mod30_s6$MTD.selection, mod30_s6$stop, mod30_s6$months),
+      c(mod60_s6$MTD.selection, mod60_s6$stop, mod60_s6$months),
+      c(modco_s6$MTD.selection, modco_s6$stop, modco_s6$months),
+      c(fit_s7$true.prob, 0, 0),
+      c(fit_s7$MTD.selection, fit_s7$stop, fit_s7$months), 
+      c(s7$summary$mtd, as.numeric(s7$summary$prob_stop[2]), s7$summary$months),
+      c(po_s7$MTD.selection, po_s7$stop, po_s7$months), 
+      c(mod30_s7$MTD.selection, mod30_s7$stop, mod30_s7$months),
+      c(mod60_s7$MTD.selection, mod60_s7$stop, mod60_s7$months),
+      c(modco_s7$MTD.selection, modco_s7$stop, modco_s7$months),
+      c(fit_s8$true.prob, 0, 0),
+      c(fit_s8$MTD.selection, fit_s8$stop, fit_s8$months), 
+      c(s8$summary$mtd, as.numeric(s8$summary$prob_stop[2]), s8$summary$months),
+      c(po_s8$MTD.selection, po_s8$stop, po_s8$months), 
+      c(mod30_s8$MTD.selection, mod30_s8$stop, mod30_s8$months),
+      c(mod60_s8$MTD.selection, mod60_s8$stop, mod60_s8$months),
+      c(modco_s8$MTD.selection, modco_s8$stop, modco_s8$months)
+      ),2)
+
+for (i in 1:nrow(SelectProb)) {
+  if(SelectProb[i,7] == 0 | is.na(SelectProb[i,7])){
+    SelectProb[i,7] <- c(' ')
+  }
+  if(SelectProb[i,8] == 0 ){
+    SelectProb[i,8] <- c(' ')
+  }
+
+}
+
+colnames(SelectProb)<- c("-1", "0", "1", "2a", "2b", "3", "Stop", "Duration")
+
+SelectProbTab <- as.matrix(cbind(
+  c('Scenario', rep('1: TD25 @-1',7), rep('2: TD25 @0',7), rep('3: TD25 @1',7),
+    rep('4: TD25 @2a',7), rep('5: TD25 @2b',7), rep('6: TD25 @3',7), 
+    rep('7: Equal steps',7), rep('8: All toxic',7)), 
+  c('CRM details', rep(c(' ', 'PO-TITE', 'TITE', 'PO', 'N = 30', 'N = 60', 
+                   'No FU'),8)),
+  c('Prior DLT', rep(c('True DLT rate', rep('P(select)',6)),8)), 
+  SelectProb))
+
+
+SelectProbTabCode <-kable(SelectProbTab, "latex", booktabs = T, linesep = "", 
+                          align = "c", caption = '\\label{tab_adept:Design_Comparison}Selection percentage of the TD25 and expected trial duration (in months) for the PO-TITE, TITE and PO CRM designs for scenarios 1-8 based on 2000 simulated trials.'
+                          ) %>% 
+  kable_styling(latex_options = c("striped", "scale_down"),
+                position = "center", font_size = 9,
+                stripe_index = c(2:8, 16:22, 30:36, 44:50)) %>% 
+  add_header_above(c(" " = 3,"Dose Levels" = 6)) %>% 
+  collapse_rows(columns = 1:2, latex_hline = "major",  valign = "middle") 
+cat(SelectProbTabCode)
