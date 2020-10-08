ac6e8903d54d000022d6b1f1b4b1224251eb6898
Amit Patel <55692909+A-mit-Patel@users.noreply.github.com>
2020-10-08 20:20:44 +0100

    Adept Chapter OC inspection
    Started writing the inspection ofOC section. Wrote two paragraphs about true DLT rates. Produced a plot in R to illustrate.

--- Thesis/Chapters/Chapter2/ADePT/Plots.R
+++ Thesis/Chapters/Chapter2/ADePT/Plots.R
@@ -29,3 +29,28 @@ ggplot(data, aes(x = Weeks, y = Weight))+
   theme_bw()
   
 
+# Plot of TRUE DLT Rates 
+
+dltrate <- c(0.25, 0.40, 0.45, 0.50, 0.55, 0.60,
+             0.12, 0.25, 0.40, 0.45, 0.50, 0.55,
+             0.09, 0.12, 0.25, 0.40, 0.45, 0.50, 
+             0.06, 0.09, 0.12, 0.25, 0.40, 0.45, 
+             0.03, 0.06, 0.09, 0.12, 0.25, 0.40, 
+             0.01, 0.03, 0.06, 0.09, 0.12, 0.25, 
+             0.05, 0.10, 0.15, 0.20, 0.25, 0.30,
+             0.50, 0.60, 0.65, 0.70, 0.75, 0.80)
+dose <- rep(c(-1,0,1,'2a','2b',3), times = 8)
+dose <- rep(1:6, times = 8)
+doselabel <- c(-1,0,1,'2a','2b',3)
+
+scenario <- rep(c('TD25@-1','TD25@0', 'TD25@1','TD25@2a','TD25@2b','TD25@3',
+                  'Equal','All toxic'), each = 6)
+data <- data.frame(dltrate, dose, scenario)
+
+ggplot(data, aes(x = dose, y = dltrate, group=scenario)) +
+  geom_line(aes(color = scenario), size = 1.5) +
+  ggtitle('True DLT rates across scenarios used for simulations') +
+  labs(x = 'Dose Level', y = 'DLT Rates', color = 'Scenario') +
+  scale_x_continuous(expand = c(0, 0), labels = doselabel) +
+  geom_hline(yintercept = 0.25, linetype = 'longdash', col = 'red') +
+  theme_bw() 
